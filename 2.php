cae924a79e30213146631bdbc876262e916f93e9
tiger-shark007 <79803895+tiger-shark007@users.noreply.github.com>
2021-05-27 00:35:51 +0300

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,68 @@
+<HTML>
+	<HEAD>
+		<TITLE> Авто подбор </TITLE>
+	</HEAD>
+	<BODY bgcolor="#FFFFFF" vlink="#014CA1">
+  <style>
+<center>
+   .leftimg {
+    float:left; /* Выравнивание по левому краю */
+    margin: 7px 7px 7px 0; /* Отступы вокруг картинки */
+   }
+   .rightimg  {
+    float: right; /* Выравнивание по правому краю  */ 
+    margin: 7px 0 7px 7px; /* Отступы вокруг картинки */
+   }
+  </style>
+
+<p> <center> <img src="FotoCar.jpg" width="70" height="50" background: #FFFFFF  class="leftimg">
+ <br>Авто подбор</p>
+  <style>
+   li {
+    display: inline-block; /* Строчно-блочные элементы */
+    background: #CA181A; /* Цвет фона */
+    margin-right: 3px; /* Расстояние между пунктами меню */
+    -webkit-transform: skewX(-30deg); /* Для Safari и Chrome */
+    -moz-transform: skewX(-30deg); /* Для Firefox */
+    -o-transform: skewX(-30deg); /* Для Opera */
+    -ms-transform: skewX(-30deg); /* Для IE */
+    transform: skewX(-30deg); /* CSS3 */
+   }
+   a {
+    color: #fff; /* Цвет ссылок */
+    display: block; /* Блочный элемент */
+    padding: 5px 15px; /* Поля вокруг текста */
+    text-decoration: none; /* Убираем подчёркивание */
+    -webkit-transform: skewX(30deg); /* Для Safari и Chrome */
+    -moz-transform: skewX(30deg); /* Для Firefox */
+    -o-transform: skewX(30deg); /* Для Opera */
+    -ms-transform: skewX(30deg); /* Для IE */
+    transform: skewX(30deg); /* CSS3 */
+   }
+   li:hover {
+    background: #333; /* Цвет фона при наведении курсора мыши */
+   }
+  </style>
+  <ul>
+    <li><a href="2.php">Подбор авто</a></li>
+    <li><a href="Glavnaj.php">Вопросы и предложения</a></li>
+  </ul>
+
+<div class="content">
+ <h1>Предоставляем вам новинки автопроизводителей, список новинок всегда будет обновляться</h1>
+ <p style="text-align:center;"> <font size="5"></font></p>
+
+<p align="CENTER"> <img src="1.jpg" alt="Машина" width="1000" height="400"</p>
+
+<p align="CENTER"> <img src="2.jpg" alt="Машина" width="1000" height="400"</p>
+
+<p align="CENTER"> <img src="3.jpg" alt="Машина" width="1000" height="400"</p>
+
+<p align="CENTER"> <img src="4.jpg" alt="Машина" width="1000" height="400"</p>
+
+</div>
+		<HR size="1" color="blue" width="80%"> <BR> </HR>
+		<p align="CENTER" size="2" fase="Verdana">  &copy; </a>  А.А. Братиков, К.Д. Баранов , 2845, 2-ка, ГУАП &ndash; 2010. Все права защищены. </p>
+	</BODY>
+</HTML>
+
